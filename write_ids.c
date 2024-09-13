a689cde01a6d5a28bc30129f8aa02d48d0cd7b2c
Mahikshit Mahikshit <mbm6865@psu.edu>
2024-09-13 14:13:43 -0400

    done

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "mbm6865";
+    char STUDENT_ID[] = "909263843";
 
     return 0 ;
 }
+
